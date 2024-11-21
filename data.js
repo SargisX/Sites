02945e197a2e6498f43a6f4662b68cc6a0830b43
SargisX <supersargis2007@gmail.com>
2024-11-21 16:41:27 +0400

    init commit

---
+++ data.js
@@ -0,0 +1,29 @@
+// data.js
+const homeworkData = [
+    {
+      img: "/img/h1.jpg",
+      title: "Homework 1",
+      description: "No Legs",
+    },
+    {
+      img: "/img/h2.jpg",
+      title: "Homework 2",
+      description: "Products",
+    },
+    {
+      img: "/img/h3.jpg",
+      title: "Homework 3",
+      description: "Flex",
+    },
+    {
+      img: "/img/h4.jpg",
+      title: "Homework 4",
+      description: "Animation",
+    },
+    {
+      img: "/img/h5.jpg",
+      title: "Homework 5",
+      description: "Grid",
+    }
+  ];
+  
