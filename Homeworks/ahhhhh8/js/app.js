41cd0c12a00fadcf6aa51ba1157fe28e118dac69
SargisX <supersargis2007@gmail.com>
2024-11-28 23:11:04 +0400

    added new version of fucking webiste for fucking Hovakimyan's dead dad

---
+++ Homeworks/ahhhhh8/js/app.js
@@ -0,0 +1,8 @@
+function saveToLocalStorage(key, value) {
+    localStorage.setItem(key, JSON.stringify(value));
+  }
+  
+  function getFromLocalStorage(key) {
+    return JSON.parse(localStorage.getItem(key));
+  }
+  
